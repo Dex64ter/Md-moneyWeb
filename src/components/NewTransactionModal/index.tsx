469c1a7b6414523bdd7021551f38a51ec56de84c
Davi J <davijosantos.dj@gmail.com>
2023-05-17 16:10:14 -0300

    Salvando dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
-import closeImg from '../../assets/botao-fechar.svg';
-import entradas from '../../assets/entradas.svg';
-import saidas from '../../assets/saidas.svg'
+import closeImg from "../../assets/botao-fechar.svg";
+import entradas from "../../assets/entradas.svg";
+import saidas from "../../assets/saidas.svg";
 import Modal from "react-modal";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,8 +14,22 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const [type, setType] = useState('deposit');
 
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+  const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      title,
+      value,
+      category,
+      type
+    })
+  }
 
   return (
     // o Modal ele pode ir em qualquer lugar da aplicação pois
@@ -26,64 +40,64 @@ export function NewTransactionModal({
       overlayClassName={"react-modal-overlay"}
       className={"react-modal-content"}
     >
-
       <button
-        type="button" 
+        type="button"
         onClick={onRequestClose}
         className="react-modal-close"
       >
-        <img
-          src={closeImg}
-          alt="Fechar janela"
-        />
+        <img src={closeImg} alt="Fechar janela" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
-        <form action="">
-          <input
-            type="text"
-            placeholder="Título"
-          />
 
-          <input
-            type="Number"
-            placeholder="Preço"
-          />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
 
-          <input
-            placeholder="Categoria"
-          />
+        <input 
+          type="Number"
+          placeholder="Preço"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
 
-          <TransactionTypeContainer>
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={entradas} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
 
-            <RadioBox
-              type="button"
-              onClick={() => {setType('deposit')}}
-              isActive={type === 'deposit'}
-              activeColor="green"
-            >
-              <img src={entradas} alt="Entrada" />
-              <span>Entrada</span>
-            </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={saidas} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
 
-            <RadioBox
-              type="button"
-              onClick={() => {setType('withdraw')}}
-              isActive={type === 'withdraw'}
-              activeColor="red"
-            >
-              <img src={saidas} alt="Saída" />
-              <span>Saída</span>
-            </RadioBox>
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
 
-          </TransactionTypeContainer>
-
-          <button type="submit">
-            Cadastrar
-          </button>
-
-        </form>
+        <button type="submit">Cadastrar</button>
+      
       </Container>
     </Modal>
   );
